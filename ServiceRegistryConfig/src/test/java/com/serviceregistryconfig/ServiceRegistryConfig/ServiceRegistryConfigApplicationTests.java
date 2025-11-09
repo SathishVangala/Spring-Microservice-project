f59c980e3c9b3a32b9caaba2f1ba11db9d6c204c
Sathishvangala1302@gmail.com <Sathish@13>
2025-11-09 13:27:09 +0530

    Add springboot folder and other untracked folders

---
+++ ServiceRegistryConfig/src/test/java/com/serviceregistryconfig/ServiceRegistryConfig/ServiceRegistryConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceregistryconfig.ServiceRegistryConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
